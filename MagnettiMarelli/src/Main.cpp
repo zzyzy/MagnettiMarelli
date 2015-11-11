160cf86e603ef0fd586c377cedf867923754c9f5
zzyzy <leezhenzhi@gmail.com>
2015-11-12 01:31:48 +0800

    Major change to project structure

---
+++ MagnettiMarelli/src/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "SQLiteCpp/SQLiteCpp.h"
+
+using namespace std;
+
+int main()
+{
+	
+
+	// Wait For User To Close Program
+	cout << "Press any key to continue . . . ";
+	cin.get();
+	return 0;
+}

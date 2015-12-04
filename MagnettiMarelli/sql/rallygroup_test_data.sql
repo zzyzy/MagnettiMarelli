7ad7a1e1daa301153327ad048299f0b58d0119c8
zzyzy <leezhenzhi@gmail.com>
2015-12-05 02:06:25 +0800

    Added test data for database

---
+++ MagnettiMarelli/sql/rallygroup_test_data.sql
@@ -0,0 +1,69 @@
+-----------------------------
+-- TEAM
+-----------------------------
+INSERT INTO Team
+VALUES (1, 'CITROEN TOTAL ABU DHABI WRT', 'YVES MATTON', 'France', 'http://www.wrc.com/en/wrc/teams/team/citroen-total-abu-dhabi-wrt/page/873-94-873--.html?h=2015/teams/2', 'Citroen DS3', 'Group R', 'Ooi Choon Ho', 'helloworld');
+INSERT INTO Team
+VALUES (2, 'F.W.R.T.', 'CARLO CASSINA', 'Italy', 'http://www.wrc.com/en/wrc/teams/team/f.w.r.t./page/873-94-873--.html?h=2015/teams/43', 'Ford Fiesta RS', 'Group R', 'Lee Zhen Zhi', 'hello123');
+INSERT INTO Team
+VALUES (3, 'HYUNDAI MOBIS WORLD RALLY TEAM', 'MICHEL NANDAN', 'Germany', 'http://www.wrc.com/en/wrc/teams/team/hyundai-mobis-world-rally-team/page/873-94-873--.html?h=2015/teams/59', 'Hyundai i20', 'Group N', 'Tan Shou Heng', 'help0982');
+INSERT INTO Team
+VALUES (4, 'HYUNDAI MOTORSPORT', 'MICHEL NANDAN', 'Germany', 'http://www.wrc.com/en/wrc/teams/team/hyundai-motorsport/page/873-94-873--.html?h=2015/teams/10', 'Hyundai i20', 'Group RGT', 'Ooi Choon Ho', 'pop1283');
+INSERT INTO Team
+VALUES (5, 'JIPOCAR CZECH NATIONAL TEAM', 'QUIRIN MULLER', 'Czech Republic', 'http://www.wrc.com/en/wrc/teams/team/jipocar-czech-national-team/page/873-94-873--.html?h=2015/teams/5', 'Ford Fiesta RS', 'Group N', 'Hoo Zheng Qi', 'oiw1231');
+INSERT INTO Team
+VALUES (6, 'M-SPORT WORLD RALLY TEAM', 'MALCOLM WILSON', 'United Kingdom', 'http://www.wrc.com/en/wrc/teams/team/m-sport-world-rally-team/page/873-94-873--.html?h=2015/teams/4', 'Ford Fiesta RS', 'Group R', 'Lee Zhen Zhi', 'kiu9283');
+INSERT INTO Team
+VALUES (7, 'RK WORLD RALLY TEAM', 'MARCIN CZACHORSKI', 'Poland', 'http://www.wrc.com/en/wrc/teams/team/rk-world-rally-team/page/873-94-873--.html?h=2015/teams/99', 'Ford Fiesta RS', 'Super 2000', 'Hoo Zheng Qi', 'jus81726');
+INSERT INTO Team
+VALUES (8, 'VOLKSWAGEN MOTORSPORT', 'JOST CAPITO', 'Germany', 'http://www.wrc.com/en/wrc/teams/team/volkswagen-motorsport/page/873-94-873--.html?h=2015/teams/1', 'Volkswagen Polo R', 'Super 2000', 'Lai Yik Sheng', 'ous1123');
+INSERT INTO Team
+VALUES (9, 'VOLKSWAGEN MOTORSPORT II', 'JOST CAPITO', 'Germany', 'http://www.wrc.com/en/wrc/teams/team/volkswagen-motorsport-ii/page/873-94-873--.html?h=2015/teams/9', 'Volkswagen Polo R', 'Group N', 'Tan Shou Heng', 'vus17263');
+INSERT INTO Team
+VALUES (10, 'PH SPORT', 'STEPHANE CLISMIS', 'France', 'https://en.wikipedia.org/wiki/Citro%C3%ABn_World_Rally_Team', 'Citroen DS3', 'Group R', 'Lai Yik Sheng', 'pieu123');
+
+-----------------------------
+-- OIC
+-----------------------------
+INSERT INTO OIC
+VALUES ('Ooi Choon Ho', 'abc123', 0);
+INSERT INTO OIC
+VALUES ('Lee Zhen Zhi', 'asd987', 0);
+INSERT INTO OIC
+VALUES ('Hoo Zheng Qi', 'okd897', 0);
+INSERT INTO OIC
+VALUES ('Tan Shou Heng', '123hsy', 0);
+INSERT INTO OIC
+VALUES ('Lai Yik Sheng', '987byd', 0);
+
+-----------------------------
+-- CAR
+-----------------------------
+INSERT INTO CAR
+VALUES ('Citroen DS3', 'Michelin');
+INSERT INTO CAR
+VALUES ('Ford Fiesta RS', 'Good Year');
+INSERT INTO CAR
+VALUES ('Volkswagen Polo R', 'Continental');
+INSERT INTO CAR
+VALUES ('Hyundai i20', 'Michelin');
+
+-----------------------------
+-- MANAGER
+-----------------------------
+INSERT INTO MANAGER
+VALUES ('YVES MATTON');
+INSERT INTO MANAGER
+VALUES ('CARLO CASSINA');
+INSERT INTO MANAGER
+VALUES ('MICHEL NANDAN');
+INSERT INTO MANAGER
+VALUES ('QUIRIN MULLER');
+INSERT INTO MANAGER
+VALUES ('MALCOLM WILSON');
+INSERT INTO MANAGER
+VALUES ('MARCIN CZACHORSKI');
+INSERT INTO MANAGER
+VALUES ('JOST CAPITO');
+INSERT INTO MANAGER
+VALUES ('STEPHANE CLISMIS');

0adedc6c6ad309957c475932d4f876f886712cad
pandaho <nonopanda.ch@gmail.com>
2015-12-05 06:15:44 +0800

    Removed unused files

--- MagnettiMarelli/src/Controller.cpp
+++
@@ -1,5 +0,0 @@
-#include <Controller.h>
-
-const std::string Controller::path = "database/MMDatabase.db";
-const int Controller::flags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE;
-SQLite::Database Controller::db(path, flags);

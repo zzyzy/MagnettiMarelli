ce1901ac6ec71323f5b4d30ea03b8a181444740d
zzyzy <leezhenzhi@gmail.com>
2015-12-05 22:27:42 +0800

    Completed SimpleDate with minimal functionality

--- MagnettiMarelliBackend/SimpleDate.cpp
+++ MagnettiMarelliBackend/SimpleDate.cpp
@@ -1,13 +1,75 @@
-#include "SimpleDate.h"
+#include <SimpleDate.h>
+
+SimpleDate::SimpleDate(const unsigned int &year, const unsigned int &month, const unsigned int &day)
+{
+	if (year < 1900 || year > 9999)
+		throw SimpleDateException("Year out of range");
+	else if (month < 1 || month > 12)
+		throw SimpleDateException("Month out of range");
+	else if (day < 1 || day > 31)
+		throw SimpleDateException("Day out of range");
+	this->year = year;
+	this->month = month;
+	this->day = day;
+}
 
 void SimpleDate::now()
 {
 	std::chrono::system_clock::time_point p = std::chrono::system_clock::now();
 	time_t t = std::chrono::system_clock::to_time_t(p);
-
+#ifdef _WIN32
+	tm timeinfo;
+	localtime_s(&timeinfo, &t);
+	year = 1900 + timeinfo.tm_year;
+	month = 1 + timeinfo.tm_mon;
+	day = timeinfo.tm_mday;
+#else
+	tm *timeinfo = localtime(&t);
+	year = std::to_string(1900 + timeinfo->tm_year);
+	month = std::to_string(1 + timeinfo->tm_mon);
+	day = std::to_string(timeinfo->tm_mday);
+#endif
 }
 
-std::string SimpleDate::to_string() const
+void SimpleDate::addDay(const unsigned int &day)
 {
-	return year + "/" + month + "/" + day;
+	this->day += day;
+	while (this->day > daysInMonths[month - 1]) {
+		if (month == 2 && isLeapYear()) {
+			this->day -= daysInMonths[month - 1] + 1;
+		}
+		else {
+			this->day -= daysInMonths[month - 1];
+		}
+		month += 1;
+		if (month > 12) {
+			month -= 12;
+			year += 1;
+		}
+	}
+}
+
+bool SimpleDate::isLeapYear() const
+{
+	if (year % 4 != 0) return false;
+	if (year % 400 == 0) return true;
+	if (year % 100 == 0) return false;
+	return true;
+}
+
+std::string SimpleDate::toString() const
+{
+	std::string simpleDate;
+	simpleDate += std::to_string(year) + "/";
+	if (std::to_string(month).length() < 2) simpleDate += "0";
+	simpleDate += std::to_string(month) + "/";
+	if (std::to_string(day).length() < 2) simpleDate += "0";
+	simpleDate += std::to_string(day);
+	return simpleDate;
+}
+
+std::ostream& operator<<(std::ostream& out, const SimpleDate &simpleDate)
+{
+	out << simpleDate.toString();
+	return out;
 }
--- MagnettiMarelliBackend/SimpleDate.h
+++ MagnettiMarelliBackend/SimpleDate.h
@@ -1,20 +1,36 @@
 #pragma once
 
+#include <iostream>
 #include <string>
 #include <chrono>
 #include <ctime>
+
+class SimpleDateException;
 
 class SimpleDate
 {
 public:
 	SimpleDate() = default;
 	~SimpleDate() = default;
+	SimpleDate(const unsigned int &year, const unsigned int &month, const unsigned int &day);
 
 	void now();
-	std::string to_string() const;
+	void addDay(const unsigned int &day);
+	bool isLeapYear() const;
+
+	std::string toString() const;
+	friend std::ostream &operator<<(std::ostream& out, const SimpleDate &simpleDate);
 
 private:
-	std::string year;
-	std::string month;
-	std::string day;
+	unsigned int year;
+	unsigned int month;
+	unsigned int day;
+	const unsigned int daysInMonths[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+};
+
+class SimpleDateException : public std::runtime_error
+{
+public:
+	SimpleDateException(const std::string &what_arg) : runtime_error(what_arg) {  };
+	~SimpleDateException() = default;
 };
